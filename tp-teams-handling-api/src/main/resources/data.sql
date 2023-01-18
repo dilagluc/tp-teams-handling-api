a8d1d88345844922c561ab01c2a0f9d0a44c980a
dilagluc <dilagluc@luc.com>
2023-01-18 14:27:32 +0100

    First commit

--- tp-teams-handling-api/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO team (name, slogan) VALUES ('PSG', 'Revons plus grand');
-INSERT INTO team (name, slogan) VALUES ('Real Madrid', 'Les galactiques');
-INSERT INTO team (name, slogan) VALUES ('Barcelone', 'La Macia');
-INSERT INTO team (name, slogan) VALUES ('Bayern', 'Les puissants en Allemagne');
-INSERT INTO team (name, slogan) VALUES ('Manchester United', 'Les reds devils');
--- tp-teams-handling-api/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE team
-(
-    id IDENTITY NOT NULL PRIMARY KEY,
-    name VARCHAR(200),
-    slogan VARCHAR(500)
-);
